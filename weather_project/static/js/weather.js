90d7bf0b9550d1b727b4571af63726769714ec74
Georgio <georgetrueman64@gmail.com>
2024-09-24 23:31:40 +0300

    update modal text danger custom.css weather.js

--- weather_project/static/js/weather.js
+++ weather_project/static/js/weather.js
@@ -81,9 +81,10 @@ $(document).ready(function () {
             'top': '50%',
             'left': '50%',
             'transform': 'translate(-50%, -50%)',
-            'background-color': '#ce9898',
+            'background-color': 'white',
             'padding': '20px',
-            'border': '2px solid red',
+            'border': '6px double #ecececb2',
+            'opacity': '0.9',
             'border-radius': '10px',
             'z-index': '1000',
             'box-shadow': '0 5px 15px rgba(0, 0, 0, 0.3)'
