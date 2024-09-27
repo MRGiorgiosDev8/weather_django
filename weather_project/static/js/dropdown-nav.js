00c0e6632b75ae845fccc9e9813b155ff38e298a
Georgio <georgetrueman64@gmail.com>
2024-09-27 18:23:06 +0300

    update line dropdown-menu

--- weather_project/static/js/dropdown-nav.js
+++ weather_project/static/js/dropdown-nav.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
     const cityDropdown = $('#cityDropdown');
+    const navLink = $('#navbarDropdown');
+    const line = navLink.find('.line');
 
-    $('#navbarDropdown').on('show.bs.dropdown', function () {
+    navLink.on('show.bs.dropdown', function () {
         gsap.fromTo(cityDropdown, {
             opacity: 0,
             height: 0,
@@ -14,6 +16,27 @@ $(document).ready(function () {
             onComplete: function () {
                 cityDropdown.css('display', '');
             }
+        });
+
+        gsap.to(line, {
+            scaleX: 1,
+            duration: 0.4,
+            ease: "power1.inOut"
+        });
+    });
+
+    navLink.on('hide.bs.dropdown', function () {
+        gsap.to(cityDropdown, {
+            opacity: 0,
+            height: 0,
+            duration: 0.3,
+            ease: "power1.inOut"
+        });
+
+        gsap.to(line, {
+            scaleX: 0,
+            duration: 0.4,
+            ease: "power1.inOut"
         });
     });
 });
